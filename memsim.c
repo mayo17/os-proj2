d013eb56dee1b8fb22b388e88b77ba32583719b5
JayceH <jayceh@mail.usf.edu>
2019-03-01 12:31:27 -0500

    made fifo a function

--- memsim.c
+++ memsim.c
@@ -14,76 +14,83 @@ int main() {
     int exists = 0;
     int pageFault = 0;
     int writes = 0;
-    while(fgets(filePage, 50, file) != NULL)
+
+    void fifo(int nframes)
     {
-        if(i < nframes)
+        while(fgets(filePage, 50, file) != NULL)
         {
-            for(j = 0; j < nframes; j++)
+            if(i < nframes)
             {
-                if(strncmp(page[j], filePage, 8) == 0)
+                for(j = 0; j < nframes; j++)
                 {
-                    exists = 1;
-                    break;
+                    if(strncmp(page[j], filePage, 8) == 0)
+                    {
+                        exists = 1;
+                        break;
+                    }
+                    else
+                    {
+                        exists = 0;
+                    }
                 }
-                else
+                if(exists == 0)
                 {
-                    exists = 0;
+                    strcpy(page[i], filePage);
+                    i++;
+                    pageFault++;
                 }
             }
-            if(exists == 0)
+            else
             {
-                strcpy(page[i], filePage);
-                i++;
-                pageFault++;
+                for(j = 0; j < nframes; j++)
+                {
+                    if(strncmp(page[j], filePage, 8) == 0)
+                    {
+                        exists = 1;
+                        break;
+                    }
+                    else
+                    {
+                        exists = 0;
+                    }
+                }
+                if(exists == 0)
+                {
+                    //If page being replaced had a write instruction increment the writes counter
+                    if(page[nframes-1][9] == 'W')
+                    {
+                        writes++;
+                    }
+                    for(j = 0; j < nframes-1; j++)
+                    {
+                        strcpy(page[j], page[j+1]);
+                    }
+                    strcpy(page[nframes-1], filePage);
+                    pageFault++;
+                }
             }
+            if(debug == 1)
+            {
+                for(i = 0; i < nframes; i++)
+                {
+                printf("%s", page[i]);
+                }
+                printf("\nNumber of Reads: %d\n", pageFault);
+                printf("Number of Writes: %d\n", writes);
+            }
+            
         }
-        else
+
+        for(i = 0; i < nframes; i++)
         {
-            for(j = 0; j < nframes; j++)
-            {
-                if(strncmp(page[j], filePage, 8) == 0)
-                {
-                    exists = 1;
-                    break;
-                }
-                else
-                {
-                    exists = 0;
-                }
-            }
-            if(exists == 0)
-            {
-                //If page being replaced had a write instruction increment the writes counter
-                if(page[nframes-1][9] == 'W')
-                {
-                    writes++;
-                }
-                for(j = 0; j < nframes-1; j++)
-                {
-                    strcpy(page[j], page[j+1]);
-                }
-                strcpy(page[nframes-1], filePage);
-                pageFault++;
-            }
-        }
-        if(debug == 1);
-        {
-            for(i = 0; i < nframes; i++)
-            {
             printf("%s", page[i]);
-            }
-            printf("\nNumber of Reads: %d\n", pageFault);
-            printf("Number of Writes: %d\n", writes);
         }
-        
+        printf("\nNumber of Reads: %d\n", pageFault);
+        printf("Number of Writes: %d\n", writes);
+
     }
 
-    for(i = 0; i < nframes; i++)
-    {
-        printf("%s", page[i]);
-    }
-    printf("\nNumber of Reads: %d\n", pageFault);
-    printf("Number of Writes: %d\n", writes);
+    fifo(nframes);
 
     return 0;
 }
