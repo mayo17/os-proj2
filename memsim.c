ddbb43705839a60367a0c6287bcb5b87d4fc15bd
JayceH <jayceh@mail.usf.edu>
2019-03-04 11:14:27 -0500

    Lru

--- memsim.c
+++ memsim.c
@@ -3,9 +3,10 @@
 
 int main() {
 
-    int nframes = 8;
+    int nframes = 12;
     char page[nframes][50];
     char filePage[50];
+    int pos = 0;
     FILE* file = fopen("sixpack.trace", "r");
     int i = 0;
     int j = 0;
@@ -90,7 +91,95 @@ int main() {
 
     }
 
-    fifo(nframes);
+    void lru(int nframes)
+    {
+        while(fgets(filePage, 50, file) != NULL)
+        {
+            if(i < nframes)
+            {
+                for(j = 0; j < nframes; j++)
+                {
+                    if(strncmp(page[j], filePage, 8) == 0)
+                    {
+                        exists = 1;
+                        pos = j;
+                        break;
+                    }
+                    else
+                    {
+                        exists = 0;
+                    }
+                }
+                if(exists == 0)
+                {
+                    strcpy(page[i], filePage);
+                    i++;
+                    pageFault++;
+                }
+            }
+            else
+            {
+                for(j = 0; j < nframes; j++)
+                {
+                    if(strncmp(page[j], filePage, 8) == 0)
+                    {
+                        exists = 1;
+                        pos = j;
+                        break;
+                    }
+                    else
+                    {
+                        exists = 0;
+                    }
+                }
+                if(exists == 0)
+                {
+                    //If page being replaced had a write instruction increment the writes counter
+                    if(page[nframes-1][9] == 'W')
+                    {
+                        writes++;
+                    }
+                    for(j = nframes-1; j > 0; j--)
+                    {
+                        strcpy(page[j], page[j-1]);
+                    }
+                    strcpy(page[0], filePage);
+                    pageFault++;
+                }
+                else
+                {
+                    for(j = pos; j > 0; j--)
+                    {
+                        strcpy(page[j], page[j-1]);
+                    }
+                    strcpy(page[0], filePage);
+                }
+                
+            }
+            if(debug == 1)
+            {
+                for(i = 0; i < nframes; i++)
+                {
+                printf("%s", page[i]);
+                }
+                printf("\nNumber of Reads: %d\n", pageFault);
+                printf("Number of Writes: %d\n", writes);
+            }
+            
+        }
+
+        for(i = 0; i < nframes; i++)
+        {
+            printf("%s", page[i]);
+        }
+        printf("\nNumber of Reads: %d\n", pageFault);
+        printf("Number of Writes: %d\n", writes);
+
+    }
+
+    lru(nframes);
+
+    //fifo(nframes);
 
     return 0;
 }
